1ff14dd20924b8caf9a4778f7f7de3f4173915a2
Ivan <off_2020@mail.ru>
2023-02-18 12:09:53 +0500

    t1

---
+++ t1/Program.cs
@@ -0,0 +1,25 @@
+﻿int i, remainder = 0, newnum = 0;
+        Console.WriteLine("Введите число: ");
+        int uinput = Convert.ToInt32((Console.ReadLine()));
+        for (i = uinput; i > 0; i = (i / 10))
+        {
+            remainder = i % 10;
+            Console.Write(remainder);
+
+            newnum = remainder;
+
+        }
+
+
+        if (newnum == uinput)
+        {
+            Console.WriteLine("Число {0} является палиндромом", uinput);
+        }
+        else
+        {
+            Console.WriteLine("Число - это не палиндром");
+        }
+        Console.WriteLine(uinput);
+        Console.WriteLine(newnum);
+        Console.ReadKey();
+        
